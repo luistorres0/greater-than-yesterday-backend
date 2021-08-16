e29cca6ae445b93afad2249b1babe429629018c3
FoolyTD <trcariaga@utexas.edu>
2021-08-16 11:35:10 -0500

    update

--- src/app.js
+++ src/app.js
@@ -1,20 +1,23 @@
+console.log("hello");
 // Require the Express package and assign it to the variable "express".
 const express = require("express");
-
+console.log("hello")
 const cors = require("cors");
 
 
 /* The Express package exports a function. When you invoke that function, 
 you get a new Express application and assign it to a variable.  */
 const app = express();
+console.log("jello");
 
 const tasksRouter = require("./tasks/tasks.router");
 
 app.use(cors());
 
 app.use(express.json());
-
+console.log("hello");
 app.use("/tasks", tasksRouter);
+console.log("hello");
 
 // Not found handler
 app.use((req, res, next) => {
